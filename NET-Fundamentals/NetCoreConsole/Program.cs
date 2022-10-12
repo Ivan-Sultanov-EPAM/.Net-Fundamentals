a79e862203c2f062206919300c3522138e178dfe
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-10-12 20:34:57 +0600

    Initial

---
+++ NET-Fundamentals/NetCoreConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetCoreConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = args.Length == 0 ? "Command line arguments were not provided" : $"Hello {string.Join(", ", args)}!";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
