cdd0d81e38626b2219addd66bfef622f6b4c593f
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-10-12 21:39:35 +0600

    Added WPF application

---
+++ NET-Fundamentals/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var caption = "Hello Message";
+            var button = MessageBoxButton.OK;
+            var icon = MessageBoxImage.None;
+
+            MessageBox.Show($"Hello, {NameTextBox.Text}", caption, button, icon, MessageBoxResult.Yes);
+        }
+    }
+}
